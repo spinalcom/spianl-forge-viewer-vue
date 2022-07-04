f52a35a3a65d12ed32d695acfdb74dfe2ee10cfb
Laurent HA TAM DAN <l.hatamdan@spinalcom.com>
2022-07-04 16:25:02 +0200

    start with no default pusible now

--- src/SpinalForgeViewerVue.vue
+++ src/SpinalForgeViewerVue.vue
@@ -1,63 +1,77 @@
+<!--
+Copyright 2022 SpinalCom - www.spinalcom.com
+
+This file is part of SpinalCore.
+
+Please read all of the following terms and conditions
+of the Free Software license Agreement ("Agreement")
+carefully.
+
+This Agreement is a legally binding contract between
+the Licensee (as defined below) and SpinalCom that
+sets forth the terms and conditions that govern your
+use of the Program. By installing and/or using the
+Program, you agree to abide by all the terms and
+conditions stated or referenced herein.
+
+If you do not agree to abide by these terms and
+conditions, do not demonstrate your acceptance and do
+not install or use the Program.
+You should have received a copy of the license along
+with this file. If not, see
+<http://resources.spinalcom.com/licenses.pdf>.
+-->
+
 <template>
-    <div id="spinal-forge-viewer"></div>
+  <div id="spinal-forge-viewer"></div>
 </template>
 
 <script>
-  import {
-    ForgeViewer
-  } from "spinal-forge-viewer";
+import { ForgeViewer } from 'spinal-forge-viewer';
 
-  export default {
-    name: "SpinalForgeViewerVue",
-    props: {
-      modelProperty: {
-        type: Object,
-        required: false,
-        validator( value ) {
-          return (value.hasOwnProperty( "isInteractive" ) &&
-            value.hasOwnProperty( "path" )) || value.hasOwnProperty( "path" )
-        }
+export default {
+  name: 'SpinalForgeViewerVue',
+  props: {
+    modelProperty: {
+      type: Object,
+      required: false,
+      validator(value) {
+        return (
+          (value.hasOwnProperty('isInteractive') &&
+            value.hasOwnProperty('path')) ||
+          value.hasOwnProperty('path')
+        );
       },
-      onInitialize: {
-        type: Function,
-        required: true
+    },
+    onInitialize: {
+      type: Function,
+      required: true,
+    },
+    headless: {
+      type: Boolean,
+      default: function () {
+        return false;
       },
-      headless: {
-        type: Boolean,
-        default: function () {
-          return false;
-        }
-      }
     },
+  },
 
-    data: function () {
-      return {
-        viewerManager: null
-      }
-    },
-    mounted() {
+  data: function () {
+    return {
+      viewerManager: null,
+    };
+  },
+  mounted() {
+    const viewerContainer = document.getElementById('spinal-forge-viewer');
 
-      const viewerContainer = document
-        .getElementById( "spinal-forge-viewer" );
+    this.viewerManager = new ForgeViewer(viewerContainer, this.headless);
+    this.viewerManager.start();
+    this.onInitialize(this.viewerManager);
+  },
 
-      this.viewerManager = new ForgeViewer( viewerContainer, this.headless );
-
-
-      if (this.modelProperty) {
-        this.viewerManager.start( this.modelProperty.path,
-          !!this.modelProperty.isInteractive );
-      }
-      this.onInitialize( this.viewerManager );
-
-    },
-
-    beforeDestroy() {
-      this.viewerManager.finish();
-    }
-
-  }
+  beforeDestroy() {
+    this.viewerManager.finish();
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
